c9695037796f4138d05d057d9b76b210ae7f3095
khalid-bensaada <kbensaada953@gmail.com>
2026-04-27 20:19:38 +0100

    added a model of comments

---
+++ app/Models/Comment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+
+
+    protected $fillable = [
+        'user_id',
+        'room_id',
+        'contentCom',
+        'rankCom',
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+}
